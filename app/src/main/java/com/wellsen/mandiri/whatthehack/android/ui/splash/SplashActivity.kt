0db570b4bfee423a602a73fffb1aeabd743e7a6e
Wellsen <wellsen79@gmail.com>
2019-07-10 12:33:55 +0700

    Add Splash screen and set as launcher

---
+++ app/src/main/java/com/wellsen/mandiri/whatthehack/android/ui/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+/*
+ * *
+ *  * Created by Wellsen on 7/10/19 12:33 PM
+ *  * for Mandiri What The Hack Hackathon
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 7/10/19 12:27 PM
+ *
+ */
+
+package com.wellsen.mandiri.whatthehack.android.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wellsen.mandiri.whatthehack.android.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_splash)
+    }
+
+}
