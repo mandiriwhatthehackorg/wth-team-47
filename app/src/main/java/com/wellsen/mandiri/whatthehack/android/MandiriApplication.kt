f316473b41c280fe2da1e767a75947428c7e8492
Wellsen <wellsen79@gmail.com>
2019-07-10 10:16:50 +0700

    Add Application class

--- app/src/main/java/com/wellsen/mandiri/whatthehack/android/MandiriApplication.kt
+++ app/src/main/java/com/wellsen/mandiri/whatthehack/android/MandiriApplication.kt
@@ -15,6 +15,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import timber.log.Timber
 
+@Suppress("unused")
 class MandiriApplication : Application() {
 
   override fun onCreate() {
