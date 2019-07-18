7fe31591a274b163a3043502d4f16f39d36cfcf5
Wellsen <wellsen79@gmail.com>
2019-07-18 10:52:14 +0700

    Fallback biometrics to keyguard

--- app/src/main/java/com/wellsen/mandiri/whatthehack/android/ui/main/MainActivity.kt
+++ app/src/main/java/com/wellsen/mandiri/whatthehack/android/ui/main/MainActivity.kt
@@ -1,17 +1,21 @@
 /*
  * *
- *  * Created by Wellsen on 7/17/19 3:36 PM
+ *  * Created by Wellsen on 7/18/19 10:52 AM
  *  * for Mandiri What The Hack Hackathon
  *  * Copyright (c) 2019 . All rights reserved.
- *  * Last modified 7/17/19 3:36 PM
+ *  * Last modified 7/18/19 10:52 AM
  *
  */
 
 package com.wellsen.mandiri.whatthehack.android.ui.main
 
+import android.app.Activity
+import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.Settings.ACTION_SECURITY_SETTINGS
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -22,12 +26,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.wellsen.mandiri.whatthehack.android.R
 import com.wellsen.mandiri.whatthehack.android.ui.login.LoginActivity
 import com.wellsen.mandiri.whatthehack.android.util.LOGGED_IN
+import com.wellsen.mandiri.whatthehack.android.util.isFingerprintAvailable
+import com.wellsen.mandiri.whatthehack.android.util.isHardwareSupported
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 import java.util.concurrent.Executors
 
+const val REQUEST_LOCK_CODE = 200
+const val REQUEST_SECURITY_SETTINGS = 201
+
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var keyguardManager: KeyguardManager
   private val sp: SharedPreferences by inject()
 
   private lateinit var textMessage: TextView
@@ -40,7 +50,7 @@ class MainActivity : AppCompatActivity() {
         }
         R.id.navigation_dashboard -> {
           textMessage.setText(R.string.title_dashboard)
-          showBiometricDialog()
+          checkBiometric("Title", "Description")
           return@OnNavigationItemSelectedListener true
         }
         R.id.navigation_notifications -> {
@@ -56,6 +66,7 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     val navView: BottomNavigationView = findViewById(R.id.nav_view)
+    keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
 
     textMessage = findViewById(R.id.message)
     navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
@@ -67,7 +78,60 @@ class MainActivity : AppCompatActivity() {
     finish()
   }
 
-  private fun showBiometricDialog() {
+  private fun checkBiometric(title: String, description: String) {
+    if (!keyguardManager.isDeviceSecure) {
+      Toast.makeText(
+        this@MainActivity,
+        "You need to activate PIN, pattern or password first", Toast.LENGTH_LONG
+      ).show()
+      val intent = Intent(ACTION_SECURITY_SETTINGS)
+      try {
+        startActivityForResult(intent, REQUEST_SECURITY_SETTINGS)
+      } catch (ex: Exception) {
+        Timber.e(ex.localizedMessage)
+      }
+      return
+    }
+
+    if (!isHardwareSupported(this)) {
+      Timber.d("Fingerprint not supported on this device")
+      showKeyguard(title, description)
+      return
+    }
+
+    if (!isFingerprintAvailable(this)) {
+      Timber.d("Fingerprint not available on this device")
+      showKeyguard(title, description)
+      return
+    }
+
+    showBiometricDialog(title, description)
+  }
+
+  private fun showKeyguard(title: String, description: String) {
+    val i = keyguardManager.createConfirmDeviceCredentialIntent(title, description)
+    try {
+      startActivityForResult(i, REQUEST_LOCK_CODE)
+    } catch (e: Exception) {
+      //If some exception occurs means Screen lock is not set up please set screen lock
+      Timber.e(e.localizedMessage)
+      Toast.makeText(
+        this@MainActivity,
+        "You need to activate PIN, pattern or password first", Toast.LENGTH_LONG
+      ).show()
+      //If some exception occurs means Screen lock is not set up please set screen lock
+      //Open Security screen directly to enable patter lock
+      val intent = Intent(ACTION_SECURITY_SETTINGS)
+      try {
+        startActivityForResult(intent, REQUEST_SECURITY_SETTINGS)
+      } catch (ex: Exception) {
+        Timber.e(ex.localizedMessage)
+      }
+
+    }
+  }
+
+  private fun showBiometricDialog(title: String, description: String) {
     val executor = Executors.newSingleThreadExecutor()
 
     val biometricPrompt = BiometricPrompt(this, executor, object : AuthenticationCallback() {
@@ -87,6 +151,7 @@ class MainActivity : AppCompatActivity() {
         super.onAuthenticationSucceeded(result)
         Timber.d("Authenticated, proceed")
         toastOnMainThread("Authenticated, proceed")
+        proceed()
       }
 
       override fun onAuthenticationFailed() {
@@ -97,9 +162,8 @@ class MainActivity : AppCompatActivity() {
     })
 
     val promptInfo = BiometricPrompt.PromptInfo.Builder()
-      .setTitle("Title")
-      .setSubtitle("Subtitle")
-      .setDescription("Description")
+      .setTitle(title)
+      .setDescription(description)
       .setNegativeButtonText("Cancel")
       .build()
 
@@ -109,6 +173,28 @@ class MainActivity : AppCompatActivity() {
   private fun toastOnMainThread(message: String) {
     runOnUiThread {
       Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+    }
+  }
+
+  private fun proceed() {
+    Timber.d("Proceed")
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+
+    if (requestCode == REQUEST_LOCK_CODE) {
+      if (resultCode == Activity.RESULT_OK) {
+        Timber.d("Authenticated, proceed")
+        proceed()
+        return
+      }
+
+      Timber.d("Not Authenticated")
+      Toast.makeText(
+        this@MainActivity,
+        "Authentication failed", Toast.LENGTH_LONG
+      ).show()
     }
   }
 
